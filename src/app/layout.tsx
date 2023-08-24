9330b4af786e5a30e6b4689e72611a673f72826e
Dawid <dawinczi06@gmail.com>
2023-08-24 23:33:23 +0200

    Fix overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="scroll-smooth overflow-x-hidden">
+      <body className={`${inter.className} overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
