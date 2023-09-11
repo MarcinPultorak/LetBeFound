e0c59ff24178f51f86668bb27635aecae311cf3b
Marcin <marcinpultorak@gmail.com>
2023-09-11 16:09:48 +0200

    our process changes

--- src/components/feature/OurProcess/OurProcess.tsx
+++ src/components/feature/OurProcess/OurProcess.tsx
@@ -22,97 +22,96 @@ const OurProcess: FC = () => {
       </h2>
       <div className="lg:grid lg:grid-flow-col lg:grid-cols-2 max-w-screen-2xl mx-auto sm:mt-20 mt-10 text-justify">
         <div />
-        <div className="space-y-4">
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-            whileInView={{
-              x: [300, 0],
-              opacity: [0, 1],
-              transition: { type: "spring", delay: 0.3 },
-            }}
-            viewport={{ once: true }}
-            className="bg-blackPurple bg-opacity-80 border border-zinc-600 rounded-2xl p-4 sm:p-8 flex flex-col items-center"
-            style={{
-              boxShadow: "0px 4px 20px 0px rgba(82, 82, 91, 0.25)",
-            }}
-          >
-            <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
-              Konsultacje i analiza
-            </h3>
-            <p className="mt-6 text-xs md:text-sm lg:text-base">
-              Rozpoczynamy naszą współpracę od dogłębnych konsultacji, aby
-              poznać Twoje cele i wymagania. Przeprowadzamy także wnikliwą
-              analizę rynku i konkurencji, aby nasze rozwiązania były
-              strategicznie dostosowane i efektywne. To nasza droga do
-              zrozumienia Twojej wizji i zbudowania solidnego fundamentu dla
-              projektu.
-            </p>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-            whileInView={{
-              x: [300, 0],
-              opacity: [0, 1],
-              transition: { type: "spring", delay: 0.6 },
-            }}
-            viewport={{ once: true }}
-            className="bg-blackPurple bg-opacity-80 border border-zinc-600 rounded-2xl p-4 sm:p-8 flex flex-col items-center"
-            style={{
-              boxShadow: "0px 4px 20px 0px rgba(82, 82, 91, 0.25)",
-            }}
-          >
-            <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
-              Projektowanie i wdrażanie
-            </h3>
-            <p className="mt-6 text-xs md:text-sm lg:text-base">
-              Nasz zespół projektowy, pełen kreatywności i pasji, przekształca
-              Twoje marzenia w rzeczywistość online. Tworzymy estetyczne i
-              intuicyjne projekty, które oddają charakter Twojej firmy. Po
-              otrzymaniu Twojej aprobaty przystępujemy do wdrożenia, dbając o
-              każdy detal, aby Twoja strona była gotowa zdobyć serca klientów.
-            </p>
-          </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.2 }}
-            whileInView={{
-              x: [300, 0],
-              opacity: [0, 1],
-              transition: { type: "spring", delay: 0.9 },
-            }}
-            viewport={{ once: true }}
-            className="bg-blackPurple bg-opacity-80 border border-zinc-600 rounded-2xl p-4 sm:p-8 flex flex-col items-center"
-            style={{
-              boxShadow: "0px 4px 20px 0px rgba(82, 82, 91, 0.25)",
-            }}
-          >
-            <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
-              Testowanie i wsparcie
-            </h3>
-            <div className="mt-6 text-xs md:text-sm lg:text-base">
-              <p>Dla nas praca nie kończy się po wdrożeniu strony:</p>
-              <ul className="list-disc my-4 list-inside space-y-1">
-                <li>
-                  Regularnie przeprowadzamy testy, by zapewnić najwyższą jakość
-                  i funkcjonalność.
-                </li>
-                <li>
-                  Wspieramy Cię także po uruchomieniu, oferując wsparcie
-                  techniczne, gdy tego potrzebujesz. Naszym celem jest
-                  zapewnienie Ci pewności, że Twoja strona działa sprawnie i
-                  skutecznie, wspierając Cię w każdym kroku na drodze do sukcesu
-                  online.
-                </li>
-                <li>
-                  W pełni zaangażowani w Twój projekt, skupiamy się na każdym
-                  etapie procesu, aby dostarczyć Ci kompleksowe i efektywne
-                  rozwiązania.
-                </li>
-              </ul>
-            </div>
-          </motion.div>
+        <div className="space-y-12 border-l-2 border-zinc-600 pl-8 sm:pl-16 h-[700px] lg:h-[1000px] xl:h-[900px] flex flex-col justify-center">
+          <div className="relative">
+            <div className="w-[15px] h-[15px] bg-orange-600 rounded-full absolute -left-[33px] sm:-left-[65px] -translate-x-1/2 translate-y-1/4" />
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+              whileInView={{
+                x: [300, 0],
+                opacity: [0, 1],
+                transition: { type: "spring", delay: 0.3 },
+              }}
+              viewport={{ once: true }}
+            >
+              <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
+                Konsultacje i analiza
+              </h3>
+              <p className="mt-6 text-xs md:text-sm lg:text-base">
+                Rozpoczynamy naszą współpracę od dogłębnych konsultacji, aby
+                poznać Twoje cele i wymagania. Przeprowadzamy także wnikliwą
+                analizę rynku i konkurencji, aby nasze rozwiązania były
+                strategicznie dostosowane i efektywne. To nasza droga do
+                zrozumienia Twojej wizji i zbudowania solidnego fundamentu dla
+                projektu.
+              </p>
+            </motion.div>
+          </div>
+
+          <div className="relative">
+            <div className="w-[15px] h-[15px] bg-orange-600 rounded-full absolute -left-[33px] sm:-left-[65px] -translate-x-1/2 translate-y-1/4" />
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+              whileInView={{
+                x: [300, 0],
+                opacity: [0, 1],
+                transition: { type: "spring", delay: 0.6 },
+              }}
+              viewport={{ once: true }}
+            >
+              <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
+                Projektowanie i wdrażanie
+              </h3>
+              <p className="mt-6 text-xs md:text-sm lg:text-base">
+                Nasz zespół projektowy, pełen kreatywności i pasji, przekształca
+                Twoje marzenia w rzeczywistość online. Tworzymy estetyczne i
+                intuicyjne projekty, które oddają charakter Twojej firmy. Po
+                otrzymaniu Twojej aprobaty przystępujemy do wdrożenia, dbając o
+                każdy detal, aby Twoja strona była gotowa zdobyć serca klientów.
+              </p>
+            </motion.div>
+          </div>
+
+          <div className="relative">
+            <div className="w-[15px] h-[15px] bg-orange-600 rounded-full absolute -left-[33px] sm:-left-[65px] -translate-x-1/2 translate-y-1/4" />
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+              whileInView={{
+                x: [300, 0],
+                opacity: [0, 1],
+                transition: { type: "spring", delay: 0.9 },
+              }}
+              viewport={{ once: true }}
+            >
+              <h3 className="text-sm sm:text-base md:text-lg text-orange-600 tracking-[.15em] underline uppercase">
+                Testowanie i wsparcie
+              </h3>
+              <div className="mt-6 text-xs md:text-sm lg:text-base">
+                <p>Dla nas praca nie kończy się po wdrożeniu strony:</p>
+                <ul className="list-disc my-4 list-inside space-y-1">
+                  <li>
+                    Regularnie przeprowadzamy testy, by zapewnić najwyższą
+                    jakość i funkcjonalność.
+                  </li>
+                  <li>
+                    Wspieramy Cię także po uruchomieniu, oferując wsparcie
+                    techniczne, gdy tego potrzebujesz. Naszym celem jest
+                    zapewnienie Ci pewności, że Twoja strona działa sprawnie i
+                    skutecznie, wspierając Cię w każdym kroku na drodze do
+                    sukcesu online.
+                  </li>
+                  <li>
+                    W pełni zaangażowani w Twój projekt, skupiamy się na każdym
+                    etapie procesu, aby dostarczyć Ci kompleksowe i efektywne
+                    rozwiązania.
+                  </li>
+                </ul>
+              </div>
+            </motion.div>
+          </div>
         </div>
       </div>
     </section>
