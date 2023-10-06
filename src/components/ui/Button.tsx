1be22ef3fe57942bd8b1904844873799a28b0c09
Dawid <dawinczi06@gmail.com>
2023-10-06 17:56:59 +0200

    button disabled fix

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,15 +7,11 @@ type Props = {
   isSubmitting?: boolean;
   variant?: "primary" | "secondary" | "orange" | "sky" | "blank";
 };
-const Button: FC<ButtonHTMLAttributes<HTMLButtonElement> & Props> = ({
-  isSubmitting,
-  ...props
-}) => {
+const Button: FC<ButtonHTMLAttributes<HTMLButtonElement> & Props> = ({ isSubmitting, ...props }) => {
   const variants: Record<string, string> = {
     blank: "",
     primary: "bg-sky-500 text-zinc-100 hover:bg-sky-800 px-5",
-    secondary:
-      "bg-white boder-black text-black border border-slate-400 hover:bg-slate-100",
+    secondary: "bg-white boder-black text-black border border-slate-400 hover:bg-slate-100",
     orange: "bg-orange-600 hover:bg-orange-500 text-zinc-100",
     sky: "bg-sky-800 hover:bg-sky-600 text-zinc-100",
   };
@@ -24,19 +20,14 @@ const Button: FC<ButtonHTMLAttributes<HTMLButtonElement> & Props> = ({
     <button
       {...props}
       className={cx(
-        "relative flex h-14 w-full items-center justify-center whitespace-nowrap rounded-md text-base font-bold uppercase transition",
+        "relative flex h-14 w-full items-center justify-center whitespace-nowrap rounded-md text-base font-bold uppercase transition disabled:bg-opacity-50 disabled:cursor-not-allowed",
         props.className,
         variants[props.variant ?? "primary"]
       )}
       disabled={props.disabled || isSubmitting}
     >
       <>
-        <Spinner
-          className={cx(
-            "absolute h-3 fill-current",
-            isSubmitting ? "visible" : "invisible"
-          )}
-        />
+        <Spinner className={cx("absolute h-3 fill-current", isSubmitting ? "visible" : "invisible")} />
         <span
           className={cx("flex h-full w-full items-center justify-center", {
             invisible: isSubmitting,
