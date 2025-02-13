f1bf505fac2312464f820f3042867fc020b03c93
Marcin <marcinpultorak@gmail.com>
2025-02-13 17:09:50 +0100

    fix

--- cms-backend/src/admin/vite.config.example.ts
+++ cms-backend/src/admin/vite.config.example.ts
@@ -1,11 +1,12 @@
-import { mergeConfig, type UserConfig } from 'vite';
+// @ts-nocheck
+import { mergeConfig, type UserConfig } from "vite";
 
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
       alias: {
-        '@': '/src',
+        "@": "/src",
       },
     },
   });
