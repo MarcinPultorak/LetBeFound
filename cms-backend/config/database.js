7e1ffe2da75dc4d7b18fd2e6974aa052214e5a05
Marcin <marcinpultorak@gmail.com>
2025-02-13 20:59:14 +0100

    ssl problem

--- cms-backend/config/database.js
+++ cms-backend/config/database.js
@@ -5,9 +5,14 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true) } : false,
+      ssl: {
+        rejectUnauthorized: false, // Akceptuj samopodpisane certyfikaty
+      },
     },
-    pool: { min: 2, max: 10 },
+    pool: {
+      min: env.int('DATABASE_POOL_MIN', 2),
+      max: env.int('DATABASE_POOL_MAX', 10),
+    },
     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
