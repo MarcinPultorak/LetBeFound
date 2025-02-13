62d82f4dda38817a7f00c64ee174d8f8b95fae2b
Marcin <marcinpultorak@gmail.com>
2025-02-14 00:22:33 +0100

    fix problem

--- cms-backend/config/server.js
+++ cms-backend/config/server.js
@@ -1,14 +1,10 @@
-module.exports = ({ env }) => {
-  console.log('Current environment:', env('NODE_ENV')); // Dodaj ten log
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-    webhooks: {
-      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
-    environment: env('NODE_ENV', 'development'), // Użyj NODE_ENV, ignoruj RAILWAY_ENVIRONMENT_NAME
-  };
-};
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
